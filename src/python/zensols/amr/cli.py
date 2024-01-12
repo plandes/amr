abf906adfbdc08fe370a40baeb5645c6acfaa0d2
Paul Landes <landes@mailc.net>
2024-01-11 21:31:53 -0600

    add app factory dumper access

--- src/python/zensols/amr/cli.py
+++ src/python/zensols/amr/cli.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 import sys
 from zensols.cli import ApplicationFactory, ActionResult, CliHarness
 from zensols.nlp import FeatureDocumentParser
+from . import Dumper
 
 
 @dataclass
@@ -24,6 +25,15 @@ class ApplicationFactory(ApplicationFactory):
         """
         harness: CliHarness = cls.create_harness()
         return harness['app'].doc_parser
+
+    @classmethod
+    def get_dumper(cls) -> Dumper:
+        """Return a object that plots and writes AMR content in human readable
+        formats.
+
+        """
+        harness: CliHarness = cls.create_harness()
+        return harness['amr_dumper']
 
 
 def main(args: List[str] = sys.argv, **kwargs: Dict[str, Any]) -> ActionResult:
