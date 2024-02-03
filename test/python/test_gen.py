271fa8fe9848a959b5569c3f3c2cfb702d7751f9
Paul Landes <landes@mailc.net>
2024-02-03 11:52:12 -0600

    fix generation error; add unit test

---
+++ test/python/test_gen.py
@@ -0,0 +1,19 @@
+from util import BaseTestApplication
+from zensols.amr import AmrFeatureDocument, GeneratedAmrDocument
+
+
+class TestGeneration(BaseTestApplication):
+    def setUp(self):
+        fac = self._get_app().config_factory
+        self.parser = fac('amr_anon_doc_parser')
+        self.gen = fac('amr_generator')
+
+    def test_gen(self):
+        s: str = 'Obama was the 44th president last year. He is no longer.'
+        doc = self.parser(s)
+        self.assertTrue(isinstance(doc, AmrFeatureDocument))
+        gdoc: GeneratedAmrDocument = self.gen(doc.amr)
+        self.assertEqual(GeneratedAmrDocument, type(gdoc))
+        should = ('Obama was the 14th president last year.',
+                  "He's no longer.")
+        self.assertEqual(should, tuple(map(lambda s: s.text, gdoc)))
