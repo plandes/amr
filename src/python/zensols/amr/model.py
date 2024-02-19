688cab3b81132baae5ab1f3bfdc9f38d7b9dc532
Paul Landes <landes@mailc.net>
2024-02-19 11:27:34 -0600

    abstract out amrlib in class and module space

--- src/python/zensols/amr/model.py
+++ src/python/zensols/amr/model.py
@@ -3,146 +3,25 @@
 """
 __author__ = 'Paul Landes'
 
-from typing import List, Tuple, Dict, Type, Callable
-from dataclasses import dataclass, field
+from typing import Tuple, Dict, Type
+from dataclasses import dataclass
+from abc import ABCMeta, abstractmethod
 import logging
-import os
-import warnings
 import json
-import textwrap as tw
-from pathlib import Path
 from spacy.language import Language
 from spacy.tokens import Doc, Span, Token
-import amrlib
-from amrlib.models.inference_bases import GTOSInferenceBase, STOGInferenceBase
-from zensols.util import loglevel
-from zensols.persist import persisted
-from zensols.install import Installer
-from zensols.nlp import FeatureDocumentParser, Component, ComponentInitializer
-from . import (
-    AmrError, AmrFailure, AmrSentence, AmrDocument,
-    AmrGeneratedSentence, AmrGeneratedDocument,
-)
+from zensols.nlp import FeatureDocumentParser
+from . import AmrError, AmrSentence, AmrDocument
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
-class ModelContainer(object):
-    """Contains an installer used to download and install a model that's then
-    used by the API to parse AMR graphs or generate language from AMR graphs.
-
-    """
-    name: str = field()
-    """The section name."""
-
-    installer: Installer = field(default=None)
-    """"Use to install the model files.  The installer must have one and only
-    resource.
-
-    """
-    alternate_path: Path = field(default=None)
-    """If set, use this alternate path to find the model files."""
-
-    def __post_init__(self):
-        # minimize warnings (T5)
-        os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-        warnings.filterwarnings(
-            'ignore',
-            message=r'^This tokenizer was incorrectly instantiated with',
-            category=FutureWarning)
-        from transformers import logging
-        logging.set_verbosity_error()
-        # save the section name since AmrParser has its ``name`` attribute
-        # replaced by the spaCy API
-        self._init_name = self.name
-
-    @property
-    def model_path(self) -> Path:
-        if self.alternate_path is None:
-            pkg_path = self.installer.get_singleton_path().parent
-        else:
-            pkg_path = self.alternate_path
-        return pkg_path
-
-    def _load_model(self) -> Path:
-        self.installer.install()
-        model_path: Path = self.model_path
-        if logger.isEnabledFor(logging.INFO):
-            logger.info(f'resolved model path: {model_path}')
-        if amrlib.defaults.data_dir != model_path:
-            if logger.isEnabledFor(logging.DEBUG):
-                logger.debug(f'setting AMR model directory to {model_path}')
-            amrlib.defaults.data_dir = model_path
-        return model_path
-
-
-@dataclass
-class AmrParser(ModelContainer, ComponentInitializer):
+class AmrParser(object, metaclass=ABCMeta):
     """Parses natural language into AMR graphs.  It has the ability to change
     out different installed models in the same Python session.
 
     """
-    add_missing_metadata: bool = field(default=True)
-    """Whether to add missing metadata to sentences when missing.
-
-    :see: :meth:`add_metadata`
-
-    """
-    model: str = field(default='noop')
-    """The :mod:`penman` AMR model to use when creating :class:`.AmrSentence`
-    instances, which is one of ``noop`` or ``amr``.  The first does not modify
-    the graph but the latter normalizes out inverse relationships such as
-    ``ARG*-of``.
-
-    """
-    def init_nlp_model(self, model: Language, component: Component):
-        """Reset the installer to all reloads in a Python REPL with different
-        installers.
-
-        """
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'initializing ({id(model)}): {self.name}')
-        doc_parser: FeatureDocumentParser = model.doc_parser
-        new_parser: AmrParser = doc_parser.config_factory(self._init_name)
-        self.installer = new_parser.installer
-
-    # if the model doesn't change after its app configuration does for the life
-    # of the interpreter, turn off caching in config amr_anon_feature_doc_stash
-    @persisted('_parse_model', cache_global=True)
-    def _get_parse_model(self) -> STOGInferenceBase:
-        """The model that parses text in to AMR graphs.  This model is cached
-        globally, as it is cached in the :mod:`amrlib` module as well.
-
-        """
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug('loading parse model')
-        model_path = self._load_model()
-        if model_path.name.find('gsii') > -1:
-            with loglevel('transformers', logging.ERROR):
-                model = amrlib.load_stog_model()
-        else:
-            model = amrlib.load_stog_model()
-        if logger.isEnabledFor(logging.INFO):
-            logger.info(f'using parse model: {model.__class__}')
-        return model, model_path
-
-    def _clear_model(self):
-        self._parse_model.clear()
-        amrlib.stog_model = None
-
-    @property
-    def parse_model(self) -> STOGInferenceBase:
-        model, prev_path = self._get_parse_model()
-        cur_path = self.model_path
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'current path: {cur_path}, prev path: {prev_path}')
-        if cur_path != prev_path:
-            self._clear_model()
-            model = self._get_parse_model()[0]
-            amrlib.stog_model = model
-        return model
-
     @staticmethod
     def is_missing_metadata(amr_sent: AmrSentence) -> bool:
         """Return whether ``amr_sent`` is missing annotated metadata.  T5 model
@@ -200,101 +79,48 @@ class AmrParser(ModelContainer, ComponentInitializer):
             amr_sent.set_metadata('ner_iob', json.dumps(iob))
         amr_sent.meta = meta
 
+    @abstractmethod
+    def annotate_amr(self, doc: Doc):
+        """Add an ``amr`` attribute to the spaCy document.
+
+        :param doc: the document to annotate
+
+        """
+        pass
+
     def __call__(self, doc: Doc) -> Doc:
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'parsing from {doc}')
-        # force load the model in to the global amrlib module space
-        stog_model: STOGInferenceBase = self.parse_model
-        # add spacy underscore data holders for the amr data structures
-        if not Doc.has_extension('amr'):
-            Doc.set_extension('amr', default=[])
-        if not Span.has_extension('amr'):
-            Span.set_extension('amr', default=[])
-        sent_graphs: List[AmrSentence] = []
-        sent: Span
-        for i, sent in enumerate(doc.sents):
-            err: AmrFailure = None
-            graphs: List[str] = None
-            try:
-                graphs = stog_model.parse_spans([sent])
-                graph: str = graphs[0]
-                if graph is None:
-                    err = AmrFailure("Could not parse: empty graph " +
-                                     f"(total={len(graphs)})", sent.text)
-                if logger.isEnabledFor(logging.INFO):
-                    graph_str = tw.shorten(str(graph), width=60)
-                    logger.info(f'adding graph for sent {i}: <{graph_str}>')
-            except Exception as e:
-                err = AmrFailure(e, sent=sent.text)
-            if err is not None:
-                sent._.amr = AmrSentence(err)
-                sent_graphs.append(sent._.amr)
-            else:
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.debug(f'creating sentence with model: {self.model}')
-                amr_sent = AmrSentence(graph, model=self.model)
-                if self.add_missing_metadata and \
-                   self.is_missing_metadata(amr_sent):
-                    self.add_metadata(amr_sent, sent, clobber=True)
-                sent._.amr = amr_sent
-                sent_graphs.append(amr_sent)
-        doc._.amr = AmrDocument(sent_graphs)
+        """See :meth:`annotate_amr`.
+
+        :param doc: the document to annotate
+
+        :return: ``doc``
+
+        """
+        self.annotate_amr(doc)
         return doc
 
 
 @dataclass
-class AmrGenerator(ModelContainer):
+class AmrGenerator(object):
     """A callable that generates natural language text from an AMR graph.
 
     :see: :meth:`__call__`
 
     """
-    use_tense: bool = field(default=True)
-    """Try to add tense information by trying to tag the graph, which requires
-    the sentence or annotations and then goes through an alignment.
-
-    :see: :class:`amrlib.models.generate_t5wtense.inference.Inference`
-
-    """
-    def __post_init__(self):
-        super().__post_init__()
-        warnings.filterwarnings(
-            'ignore',
-            message=r'^`num_beams` is set to 1. However, `early_stopping` is ',
-            category=UserWarning)
-
-    @property
-    @persisted('_generation_model', cache_global=True)
-    def generation_model(self) -> GTOSInferenceBase:
-        """The model that generates sentences from an AMR graph."""
-        logger.debug('loading generation model')
-        self._load_model()
-        return amrlib.load_gtos_model()
-
-    def __call__(self, doc: AmrDocument) -> Tuple[str]:
-        """Generate a sentence from a spaCy document.
+    @abstractmethod
+    def generate(self, doc: AmrDocument) -> Tuple[str]:
+        """Generate a sentence from the AMR graph ``doc``.
 
         :param doc: the spaCy document used to generate the sentence
 
         :return: a text sentence for each respective sentence in ``doc``
 
         """
-        model: GTOSInferenceBase = self.generation_model
-        generate_fn: Callable = model.generate
-        # upgrade to amrlib 0.8.0
-        if 0:
-            from amrlib.models.generate_t5wtense.inference \
-                import Inference as T5TenseInference
-            if isinstance(model, T5TenseInference):
-                org_fn: Callable = generate_fn
-                generate_fn = (lambda s: org_fn(s, use_tense=self.use_tense))
-        preds: Tuple[List[str], List[bool]] = generate_fn(list(map(
-            lambda s: s.graph_string, doc)))
-        sents: List[AmrGeneratedSentence] = []
-        sent: AmrSentence
-        for sent, (text, clipped) in zip(doc, zip(*preds)):
-            sents.append(AmrGeneratedSentence(text, clipped, sent))
-        return AmrGeneratedDocument(sents=sents, amr=doc)
+        pass
+
+    def __call__(self, doc: AmrDocument) -> Tuple[str]:
+        """See :meth:`generate`."""
+        return self.generate(doc)
 
 
 @Language.factory('amr_parser')
