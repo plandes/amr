2e7f1d867738e50776944f40f92d9cea160f37c0
Paul Landes <landes@mailc.net>
2024-04-25 17:04:31 -0500

    fix dup top node; add full LP corpus test; doc

--- src/python/zensols/amr/varidx.py
+++ src/python/zensols/amr/varidx.py
@@ -7,7 +7,6 @@ from typing import Dict, Tuple, List, Sequence, Any, Set, Union
 from dataclasses import dataclass
 import logging
 import collections
-from itertools import chain
 from penman.graph import Graph, Instance, Attribute, Edge
 from penman.layout import Push
 from . import AmrError, AmrSentence
@@ -54,13 +53,13 @@ class VariableIndexer(object):
         return index
 
     def _create_sent_repls(self, c_ix: Dict[str, Tuple[int, Instance]]) -> \
-            Dict[int, Dict[str, Tuple[str, Instance]]]:
+            Dict[int, Dict[str, str]]:
         """Create by sentence triple replacements.
 
         :param c_ix: index of single letter non-indexed variable
 
         :return: a mapping of sentence index to a sentence level mapping of
-                 original (source) indexed varaible name
+                 original (source) to replacement variable name
 
         """
         s_ix: Dict[int, Dict[str, Tuple[str, Instance]]] = \
@@ -83,12 +82,14 @@ class VariableIndexer(object):
         """Rename the variables and replace graphs for those that have at least
         one replacement.
 
-        :param s_ix: sentence triple replacements
+        :param s_ix: sentence triple replacements (see
+                     :meth:`_create_sent_repls`)
 
         :param sents: the AMRs that are to be modified
 
         """
-        def map_attr(vals: List[Any], repl: Dict[str, str]):
+        def map_epi(vals: List[Any], repl: Dict[str, str]):
+            """Replace variables in epigraph data."""
             repls: List[Any] = []
             for o in vals:
                 if isinstance(o, Push):
@@ -98,27 +99,42 @@ class VariableIndexer(object):
                 repls.append(o)
             return repls
 
+        # iterate through sentence triple replacements
         six: int  # sentence index
         repls: Dict[int, Dict[str, str]]  # replacements
         for six, repls in s_ix.items():
             sent: AmrSentence = sents[six]
             g: Graph = sent.graph
+            # the triples to replace in the new graph
             repl_trips: List[Tuple[Tuple[str, str, str],
                                    Tuple[str, str, str]]] = []
+            # number of replacements made for the sentence
             n_repls: int = 0
-            nodes: Set[Union[Instance, Attribute]] = \
-                set(chain.from_iterable((g.instances(), g.attributes())))
+            # concepts
+            insts: Set[Instance] = set(g.instances())
+            # concepts and attributes
+            nodes: Set[Union[Instance, Attribute]] = insts | set(g.attributes())
+            # edges/roles
             edges: Set[Edge] = set(g.edges())
-            top: Tuple[str, str, str] = None
+            # the variable of the top node
+            top: str = None
+            # iterate over original (currently graph) triples
             otrip: Tuple[str, str, str]
             for otrip in g.triples:
+                # use the replacement triple to the original if no change
                 rtrip: Tuple[str, str, str] = otrip
+                # if a node, create a new triple with the varaible replacement
                 if otrip in nodes:
-                    src: str = otrip[0]
-                    targ: str = repls.get(src)
-                    if g.top == src:
-                        assert top is None
-                        top = otrip[0] if targ is None else targ
+                    src: str = otrip[0]  # original variable
+                    targ: str = repls.get(src)  # replacement target variable
+                    # remember the top graph concept as we iterate past it
+                    if g.top == src and otrip in insts:
+                        # the top triple is original if no replacement is given
+                        next_top: str = otrip[0] if targ is None else targ
+                        if top is not None:
+                            raise AmrError(
+                                f'Duplicate top found: {top}, {next_top}', sent)
+                        top = next_top
                     if targ is not None:
                         rtrip = (targ, *otrip[1:])
                         n_repls += 1
@@ -135,16 +151,23 @@ class VariableIndexer(object):
                             logger.debug(f'e-s{six}: {otrip} -> {rtrip}')
                         n_repls += 1
                 else:
-                    raise AmrError(f'Not a concept, attribute or edge: {otrip}')
+                    raise AmrError(f'Not a concept, attribute or edge: {otrip}',
+                                   sent)
                 repl_trips.append((otrip, rtrip))
+            # only create/replace a new graph if there's at least one replace
             if n_repls > 0:
                 if logger.isEnabledFor(logging.DEBUG):
                     logger.debug(f'sent {six} updates: {n_repls}')
-                assert top is not None
+                # make sure foudn the top concept
+                if top is None:
+                    raise AmrError('Top concept never found', sent)
+                # original (current) epigraph
                 oepis: Dict[Attribute, List] = g.epidata
+                # create the replacement epigraph
                 repis: Dict[Attribute, List] = dict(map(
-                    lambda t: (Attribute(*t[1]), map_attr(oepis[t[0]], repls)),
+                    lambda t: (Attribute(*t[1]), map_epi(oepis[t[0]], repls)),
                     repl_trips))
+                # create and set the new graph on the sentence
                 sent.graph = Graph(
                     triples=tuple(map(lambda t: t[1], repl_trips)),
                     top=top,
@@ -158,8 +181,11 @@ class VariableIndexer(object):
         :param sents: sentences whose variables will be reindexed
 
         """
+        # index concepts
         c_ix: Dict[str, Tuple[int, Instance]] = \
             self._get_concept_index(sents)
-        s_ix: Dict[int, Dict[str, Tuple[str, Instance]]]
-        s_ix = self._create_sent_repls(c_ix)
+        # compile concept variable replacements
+        s_ix: Dict[int, Dict[str, Tuple[str, Instance]]] =\
+            self._create_sent_repls(c_ix)
+        # make the replacements
         self._replace_graphs(s_ix, sents)
--- test/python/test_varidx.py
+++ test/python/test_varidx.py
@@ -1,4 +1,6 @@
+from typing import Dict
 import sys
+import collections
 from io import StringIO
 from pathlib import Path
 from zensols.amr import AmrDocument
@@ -8,13 +10,50 @@ from util import BaseTestApplication
 class TestReindex(BaseTestApplication):
     def setUp(self):
         self.maxDiff = sys.maxsize
+        self.should_file: Path = Path('test-resources/lp-reindexed.txt')
 
-    def test_reindex(self):
+    def _get_doc(self) -> AmrDocument:
+        return AmrDocument.from_source(Path('test-resources/lp.txt'))
+
+    def _test_reindex_unique(self, doc, max_cnt: int, should: Dict[str, int]):
+        debug: bool = 0
+        cnts: Dict[str, int] = collections.defaultdict(lambda: 0)
+        for sent in doc.sents:
+            for var in sent.graph.variables():
+                cnts[var] += 1
+        if debug:
+            from pprint import pprint
+            pprint(dict(cnts))
+        self.assertEqual(max_cnt, max(cnts.values()))
+        if should is None:
+            should = [1] * len(cnts)
+            self.assertEqual(should, list(cnts.values()))
+        else:
+            for k, v in should.items():
+                self.assertEqual(v, cnts[k])
+
+    def test_reindex_unique(self):
+        doc: AmrDocument = self._get_doc()
+        self._test_reindex_unique(doc, 3, dict(p=3))
+        doc.reindex_variables()
+        self._test_reindex_unique(doc, 1, None)
+
+    def test_reindex_all_lp(self):
+        app = self._get_app()
+        inst = app.config_factory('amr_anon_corpus_installer')
+        lp: Path = inst.get_singleton_path()
+        inst()
+        self.assertTrue(lp.is_file())
+        doc = AmrDocument.from_source(lp)
+        self._test_reindex_unique(doc, 721, dict(p=531))
+        doc.reindex_variables()
+        self._test_reindex_unique(doc, 1, None)
+
+    def test_reindex_by_write(self):
         write: bool = 0
         write_formatted: bool = 0
 
-        should_file: Path = Path('test-resources/lp-reindexed.txt')
-        doc = AmrDocument.from_source(Path('test-resources/lp.txt'))
+        doc: AmrDocument = self._get_doc()
         if write_formatted:
             for s in doc.sents:
                 s.graph
@@ -28,8 +67,8 @@ class TestReindex(BaseTestApplication):
         doc.write(writer=sio)
         actual: str = sio.getvalue()
         if write:
-            with open(should_file, 'w') as f:
+            with open(self.should_file, 'w') as f:
                 f.write(actual)
-        with open(should_file) as f:
+        with open(self.should_file) as f:
             should = f.read()
         self.assertEqual(should, actual)
